4a449272cdecfbcf558a50dcd76057b31bd4d1b9
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-06-18 19:38:01 +0400

    add catalogs archiving

--- catalogs.sh
+++ catalogs.sh
@@ -2,27 +2,50 @@
 
 catalogs=$1
 files=$2
-shoud_reset=$3
+make_archive=$3
+shoud_reset=$4
 
 create_files () {
     for (( file=1; file <= $files; file++ ))
     do
-        touch "$1/${file}"
+        path="$1/${file}"
+        touch $path
+        echo $path
     done
 }
 
-for (( catalog=1; catalog <= $catalogs; catalog++ ))
-do
-    if [ $shoud_reset = "true" ]
-    then
-        rm -r $catalog
-    fi
-    
-    if [ -d $catalog ]
-    then
+create_catalogs () {
+    for (( catalog=1; catalog <= $catalogs; catalog++ ))
+    do
+        if [ $shoud_reset = "true" ]
+        then
+            rm -r $catalog
+        fi
+        
+        if ! [ -d $catalog ]
+        then
+            mkdir $catalog
+        fi
+        
         create_files $catalog
-    else
-        mkdir $catalog
-        create_files $catalog
-    fi
-done
+    done
+}
+
+create_archive () {
+    for file in $files
+    do
+        if [ -e "archive.tar.gz" ]
+        then
+            tar -cvf archive.tar.gz $file
+        else
+            tar -rvf archive.tar.gz $file
+        fi
+    done
+}
+
+files=$(create_catalogs)
+
+if [ $make_archive = "true" ]
+then
+    create_archive $files
+fi
